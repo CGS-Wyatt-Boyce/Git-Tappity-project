8ba21f171b300fbdb91a741fe0e5a87ef9fc713d
Wyatt A. Boyce <Wyatt@KMZFX-XOXZ.local>
2017-05-29 19:35:02 +1000

    dev 1

--- Tappity 2/ViewController.swift
+++ Tappity 2/ViewController.swift
@@ -12,30 +12,54 @@ class ViewController: UIViewController {
     
     @IBOutlet var time: UILabel!
     @IBOutlet var score: UILabel!
+    @IBOutlet var highScoreDisplay: UILabel!
+    @IBOutlet var endGame: UIAlertAction!
+   
     
     var  taps = 0
     var count = 30
+    var gameStarted = true
+    var highScores = [String()]
     
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+            }
+    func alert() {
+        
+    }
+    
+    func gameHasStarted() {
+        if gameStarted == true {
+            gameStarted = false
+        }
+        print(gameStarted)
+        let _  = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)  { (timer) in
+            self.count -= 1
+            self.time.text = String(self.count)
+            
+            if self.count == 0 {
+                timer.invalidate()
+                self.taps = 0
+                self.count = 30
+                self.gameStarted = true
+                self.highScores.append(self.score.text!)
+                //self.highScoreDisplay.text = self.highScores
+            }
+        }
+    }
+
     
     @IBAction func buttonTapped(sender: UIButton) {
         taps += 1
         score.text = String(taps)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        let _  = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false)  { (timer) in
-            self.count -= 1
-            
-            if self.count == 0 {
-                timer.invalidate()
-                
-            }
+        if gameStarted == true {
+            gameHasStarted()
         }
-
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
